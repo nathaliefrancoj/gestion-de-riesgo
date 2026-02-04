bab2d91acd7591e01dae5f517dab9dfa938e744a
Nathalie Franco <168150760+nathaliefrancoj@users.noreply.github.com>
2026-02-03 21:22:51 -0500

    Update trading_system.py

--- trading_system.py
+++ trading_system.py
@@ -44,6 +44,11 @@ th, td {
     font-weight: bold;
 }
 
+.text-win,
+.text-loss {
+    border-radius: 6px;
+}
+
 .bold { font-weight: bold; }
 
 /* -------- BOTONES UNIFICADOS -------- */
@@ -289,6 +294,7 @@ if st.session_state.hist:
 else:
 
     st.markdown("<div class='empty-box'>Aún no hay operaciones registradas</div>", unsafe_allow_html=True)
+
 
 
 
